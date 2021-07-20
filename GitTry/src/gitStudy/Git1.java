a0917332d57399368f02368d911dc23b7bb2e2e1
DRK <DRK@DRKAERO>
2021-07-20 20:50:54 +0530

    update is done by second user.....method2 is added

--- GitTry/src/gitStudy/Git1.java
+++ GitTry/src/gitStudy/Git1.java
@@ -5,4 +5,7 @@ public class Git1 {
 	public static void method1() {
 		System.out.println("hi i m first");
 	}
+	public static void method2() {
+		System.out.println(" hi i m added by second user");
+	}
 }
